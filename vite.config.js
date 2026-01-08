678678051bbe8a8bba806103251af4fd76c48d57
sahoo-tech <ss9830872697@gmail.com>
2026-01-08 19:33:31 +0530

    Fix GitHub Pages: use relative base path and add .nojekyll

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Khata/',
+  base: './',
   server: {
     port: 3000,
     open: true
   },
   build: {
     outDir: 'dist',
-    sourcemap: true
+    sourcemap: false,
+    assetsDir: 'assets'
   }
 })
